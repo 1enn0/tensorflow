48cdfafa8a5c1796c513499905ede1804b5f0119
Lennart Hannink <l.hannink@steinberg.de>
2019-07-02 11:05:37 +0200

    [update] custom op examples

--- tensorflow/examples/my_custom_op/main.cc
+++ tensorflow/examples/my_custom_op/main.cc
@@ -24,29 +24,104 @@ limitations under the License.
 #include "tensorflow/cc/ops/standard_ops.h"
 #include "tensorflow/cc/client/client_session.h"
 
-int main(int argc, char* argv[]) {
-  using namespace tensorflow;
-  using namespace tensorflow::ops;
+namespace {
+using namespace tensorflow;
+using namespace tensorflow::ops;
 
+//-----------------------------------------------------------------------------
+void lookupUsingGather()
+{
   Scope root = Scope::NewRootScope();
 
-  auto ops = OpRegistry::Global()->DebugString(false);
-  const string opsFilePath {"/tmp/ops.pbtxt"};
-  WriteStringToFile(Env::Default(), opsFilePath, ops);
+  // approach 2: using const tensor
+  /* auto lut = Const(root.WithOpName("lut"), {100.f, 101.f, 102.f, 103.f}); */
+  auto lut = Variable(root, {4}, DT_FLOAT);
+  auto assign_lut = Assign(root, lut, RandomUniform(root, {4}, DT_FLOAT));
+  auto keys = Input({3, 2, 1, 0, 0, 1, 1, 1, 1, 2, 3, 2, 1, 0, 0, 1});
+  auto res = Sum(root, Gather(root, lut, keys), 0);
+
+  ClientSession session {root};
+
+  std::vector<Tensor> outputs {};
+  TF_CHECK_OK (session.Run({assign_lut}, nullptr));
+  TF_CHECK_OK (session.Run({res}, &outputs));
+  LOG(INFO) << "lookupUsingGather(): outputs[0].shape() = " <<  outputs[0].shape() << "\n" << outputs[0].flat<float>();
+}
+
+//-----------------------------------------------------------------------------
+void lookupUsingHashTable()
+{
+  Scope root = Scope::NewRootScope();
+
+  // approach 1: using hash table
+  auto lut = MutableHashTable(root, DT_INT32, DT_FLOAT, MutableHashTable::SharedName("lut"));
+  auto keys = Input({0, 1, 2, 3});
+  auto values = Input({100.f, 101.f, 102.f, 103.f});
+  auto lut_insert = LookupTableInsert(root, lut, keys, values);
+
+  auto find_keys = Input({3, 2, 1, 0});
+  auto res = Sum(root, LookupTableFind(root, lut, find_keys, -1.f), 0);
+
+  ClientSession session {root};
+
+  // initialize table by running insert op
+  TF_CHECK_OK (session.Run({}, {}, {lut_insert}, nullptr));
+
+  // fetch outputs
+  std::vector<Tensor> outputs {};
+  TF_CHECK_OK (session.Run({res}, &outputs));
+  LOG(INFO) << "lookupUsingHashTable(): outputs[0].shape() = " <<  outputs[0].shape() << "\n" << outputs[0].flat<float>();
+}
+
+//-----------------------------------------------------------------------------
+void lookupUsingMyCustomOp()
+{
+  Scope root = Scope::NewRootScope();
 
   auto a = Const(root.WithOpName("a"), {uint8(0), uint8(1), uint8(2), uint8(3)});
   auto b = Const(root.WithOpName("b"), {uint8(0), uint8(1), uint8(2), uint8(3)});
-  auto res = DotLUT(root.WithOpName("dotLUT"), a, b);
+  auto res = DotProductLut(root.WithOpName("dotLUT"), a, b);
+  auto lut = Variable(root, {4, 4}, DT_FLOAT);
+  auto assign_lut = Assign(root.WithOpName("assign_lut"), lut, RandomUniform(root, {4, 4}, DT_FLOAT));
 
-  std::vector<Tensor> outputs {};
   ClientSession session {root};
-  TF_CHECK_OK (session.Run({res}, &outputs));
-  LOG(INFO) << "outputs[0].shape() = " <<  outputs[0].shape() << "\n" << outputs[0].vec<float>();
 
+  // initialize lut variable by running assign op
+  TF_CHECK_OK (session.Run({assign_lut}, nullptr));
+
+  // fetch outputs
+  std::vector<Tensor> outputs {};
+  TF_CHECK_OK (session.Run({res}, &outputs));
+  LOG(INFO) << "lookupUsingMyCustomOp(): outputs[0].shape() = " <<  outputs[0].shape() << "\n" << outputs[0].flat<float>();
+}
+
+//-----------------------------------------------------------------------------
+void writeOpsToFile () 
+{
+  auto ops = OpRegistry::Global()->DebugString(false);
+  const string opsFilePath {"/tmp/ops.pbtxt"};
+  WriteStringToFile(Env::Default(), opsFilePath, ops);
+}
+
+//-----------------------------------------------------------------------------
+void writeGraphToTextFile (const Scope& root)
+{
   GraphDef graph_def;
   root.ToGraphDef(&graph_def);
   const string graph_file_path {"/tmp/graph.pbtxt"};
   WriteTextProto(Env::Default(), graph_file_path, graph_def);
+}
+
+//-----------------------------------------------------------------------------
+} // anonymous
+
+//-----------------------------------------------------------------------------
+int main(int argc, char* argv[]) 
+{
+  lookupUsingGather();
+  lookupUsingHashTable();
+  lookupUsingMyCustomOp();
 
   return 0;
 }
+
